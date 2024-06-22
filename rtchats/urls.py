e78d31c0f843a63f30cba750da9a7cdf839af6b6
cole <mathlover1998@gmail.com>
2024-06-22 21:47:09 +0700

    1. only verified email can join the group chat 2. add chatroom_edit_view function that edit groupchat name and members

--- rtchats/urls.py
+++ rtchats/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('chat/new_groupchat',create_groupchat,name='new_groupchat'),
     path('chat/<username>',get_or_create_chatroom,name='start-chat'),
     path('chat/room/<chatroom_name>',chat_view,name='chatroom'),
+    path('chat/edit/<chatroom_name>',chatroom_edit_view,name='edit-chatroom'),
+    path('chat/delete/<chatroom_name>',chatroom_edit_view,name='chatroom-delete')
 ]
--- rtchats/views.py
+++ rtchats/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.urls import reverse
 from django.http import Http404
+from django.contrib import messages
 from .models import *
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def chat_view(request,chatroom_name ='public-chat'):
     chat_group = get_object_or_404(ChatGroup,group_name=chatroom_name)
-    messages = chat_group.chat_messages.all()[:30]
+    chat_messages = chat_group.chat_messages.all()[:30]
     other_user = None
     if chat_group.is_private:
         if request.user not in chat_group.members.all():
@@ -18,7 +19,10 @@ def chat_view(request,chatroom_name ='public-chat'):
                 break
     if chat_group.groupchat_name:
         if request.user not in chat_group.members.all():
-            chat_group.members.add(request.user)
+            if request.user.emailaddress_set.filter(verified=True).exists():
+                chat_group.members.add(request.user)
+            else:
+                messages.warning(request,"You need to verify your email to join the chat!")
     
     #does not need anymore when using websocket
     # if request.htmx:
@@ -31,7 +35,7 @@ def chat_view(request,chatroom_name ='public-chat'):
     #     return render(request,'rtchat/partials/chat_message_p.html',context)
         
     context = {
-        'chat_messages':messages,
+        'chat_messages':chat_messages,
         'other_user': other_user,
         'group_name': chatroom_name,
         'chat_group':chat_group
@@ -68,3 +72,25 @@ def create_groupchat(request):
         # }
         return redirect('chatroom',new_groupchat.group_name)
     return render(request,'rtchat/create_groupchat.html')
+
+def chatroom_edit_view(request,chatroom_name):
+    chat_group = get_object_or_404(ChatGroup,group_name=chatroom_name)
+    if request.user != chat_group.admin:
+        raise Http404()
+    if request.method =='POST':
+        chat_group.groupchat_name = request.POST.get('groupchat_name')
+        chat_group.save()
+        remove_members = request.POST.getlist('remove_members')
+        for member_id in remove_members:
+            member = User.objects.get(id = member_id)
+            chat_group.members.remove(member)
+        return redirect('chatroom',chatroom_name)
+        
+    return render(request,'rtchat/chatroom_edit.html',{'chat_group':chat_group})
+
+# def invite_member(request):
+#     return render(request,'rtchat/')
+
+# def remove_member(request):
+
+#     return redirect()
